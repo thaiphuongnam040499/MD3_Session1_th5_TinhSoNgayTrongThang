72adddf46a75622ec1d4bf0cf23525e7d1a7118d
thaiphuongnam040499 <thainamvinhuni@gmail.com>
2023-03-07 10:53:01 +0700

    th5

---
+++ src/TinhSoNgayTrongThang.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class TinhSoNgayTrongThang {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhap thang muon tinh ngay:");
+        int month = scanner.nextInt();
+        switch (month){
+            case 2:
+                System.out.println("Thang "+month+" co 28 hoac 29 ngay!");
+                break;
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                System.out.println("Thang "+month+" co 31 ngay!");
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                System.out.println("Thang "+month+" co 30 ngay!" );
+                break;
+            default:
+                System.out.println("Khong co thang nay");
+        }
+    }
+}
